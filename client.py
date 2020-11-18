9bf3942ad5a0743efe524fc923305a93b0f86d12
Fedor Kosolapov <fedor_coder@mail.ru>
2020-11-18 14:42:13 +0700

    create project`s skeleton

--- client.py
+++ client.py
@@ -1,7 +1,43 @@
+from typing import Tuple, Optional, Callable
+
+
+def command():
+    return 'OK'
+
+
+def parse_command(input_command: str) -> Tuple[str, Optional[Callable]]:
+    if input_command not in ['set', 'get']:
+        return 'Unknown command', None
+    return '', command
+
+
+def head():
+    print('-'*50)
+    print('** Client for In-memory key-value storage')
+    print('** Available commands:')
+    print('** exit')
+    print('** set key=value key2=value ...')
+    print('** get key1 key2 ...')
+    print('** del key1 key2 ...')
+    print('** list')
+    print('** clear')
+    print('-'*50)
 
 
 def main():
-    pass
+    input_command = ''
+    head()
+    while True:
+        input_command = input('>')
+        if input_command == 'exit':
+            break
+        error_message, command = parse_command(input_command)
+        if error_message:
+            print(error_message)
+        else:
+            message = command()
+            print(message)
+
 
 if __name__ == "__main__":
     main()
